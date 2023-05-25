646d43bd269176db1d10292588e26726436cb0a8
Raashid Naik <rx.naik@gmail.com>
2023-05-25 19:01:57 +0000

    Committing Lesson 2 files

---
+++ Lesson2/pseudocodepractice.rb
@@ -0,0 +1,46 @@
+# Exercise 1
+=begin
+
+START
+
+# A method that returns the sum of two integers
+
+GET number1
+GET number2
+
+SET sum = number1 + number2
+
+PRINT sum
+=end
+
+# Exercise 2
+
+=begin
+START
+
+# Given an array of strings
+
+SET string_array = ["sand", "water", "beach", "fun", "sun"]
+
+WHILE stringe_array each string
+  SET concatenate = string_array[string]
+
+PRINT concatentate
+=end
+
+# Exercise 3
+
+=begin
+START
+
+SET array = [1, 4, 7, 2, 5]
+
+WHILE array each number
+  SET every_other = []
+  IF array[index] is odd
+    next
+  ELSE
+    SET every_other to add array number
+
+
+=end
