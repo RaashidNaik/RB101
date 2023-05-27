1c797faf3e63ffe0e469c0b834e172dc044377c0
Raashid Naik <rx.naik@gmail.com>
2023-05-27 13:17:00 +0000

    Updated Calculator with 1)Better Integer Validation 2)Number Validation 3)Case Return method in operation_to_message 4)Created configuration file calculator_messages.yml for all messages communicated to user

--- Lesson2/calculator.rb
+++ Lesson2/calculator.rb
@@ -1,108 +1,119 @@
 # Ask the user for two numbers
-# Ask the user to for an operation to perform 
+# Ask the user to for an operation to perform
 # Perform the operation on the two numbers
 # Output the result
+
+require 'yaml'
+
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 
 def prompt(message)
   Kernel.puts(" => #{message}")
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def valid_integer?(num)
+  num.to_i.to_s == num
+end
+
+def valid_float?(num)
+  num.to_f.to_s == num 
 end
 
 def operation_to_message(op)
-  case op 
-    when '1'
-      'Adding'
-    when '2'
-      'Subtracting'
-    when '3'
-      'Multiplying'
-    when '4'
-      'Dividing'
-    end
-end
+  word = case op
+         when '1'
+          'addop'
+         when '2'
+          'subop'
+         when '3'
+          'mulop'
+         when '4'
+          'divop'
+         end
+  # Random line of code here 
   
+  return word
+end
 
-prompt("Welcome to Calculator! Enter your name")
+
+prompt(MESSAGES['welcome']['eng'])
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
-  
+
   if name.empty?()
-    prompt("Make sure to use a valid name")
+    prompt(MESSAGES['validname']['eng'])
   else
     break
   end
-
 end
 
 number1 = ''
 number2 = ''
-loop do # the main loop
+loop do# the main loop
   loop do 
-    prompt("What's the first number?")
+    prompt(MESSAGES['firstnum']['eng'])
     number1 = Kernel.gets().chomp()
-    
-    if valid_number?(number1)
-      break
-    else
-      prompt("That doesn't look valid")
-    end
-  end
-  
-  loop do 
-    prompt("What's second number?")
-    number2 = Kernel.gets().chomp()if valid_number?(number1)
-    if valid_number?(number2)  
-      break
-    else
-      prompt("That doesn't look valid")
-    end
-  end
-  
-  operator_prompt = <<-MSG
-    What operation would you like to perform? 
-    1) add 
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-  
-  
-  prompt(operator_prompt)
-  
-  operator = ''
-  
-  loop do 
-    operator = Kernel.gets().chomp()
-    
-    if %(1 2 3  4).include?(operator)
-      break
-    else
-      prompt("Must choose 1, 2, 3, or 4")
-    end
-  end
-  
-  prompt("#{operation_to_message(operator)} the two numbers...")
-  
-  result = case operator
-    when '1'
-    result = number1.to_i() + number2.to_i()
-    when  '2'
-    result = number1.to_i() - number2.to_i()
-    when '3'
-    result = number1.to_i() * number2.to_i()
-    when '4'
-    result = number1.to_f() / number2.to_f()
-  end
-  
-  prompt("The result is #{result}")
-  
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
-  answer =  Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
 
+    if valid_integer?(number1)
+      number1 = number1.to_i
+      break
+    elsif valid_float?(number1)
+      number1 = number1.to_f
+      break
+    else
+      prompt(MESSAGES['validnum']['eng'])
+    end
+  end
+
+  loop do
+    prompt(MESSAGES['secondnum']['eng'])
+    number2 = Kernel.gets().chomp()
+    if valid_integer?(number2)
+      number2 = number2.to_i
+      break
+    elsif valid_float?(number2)
+      number2 = number2.to_f
+      break
+    else
+      prompt(MESSAGES['validnum']['eng'])
+    end
+  end
+
+  operator_prompt = MESSAGES['opmessage']['eng']
+
+  prompt(operator_prompt)
+
+  operator = ''
+
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %(1 2 3 4).include?(operator)
+      break
+    else
+      prompt(MESSAGES['validop']['eng'])
+    end
+  end
+
+  prompt("#{MESSAGES[operation_to_message(operator)]['eng']} #{MESSAGES['twonums']['eng']}")
+
+  result = case operator
+  when '1'
+    result = number1 + number2
+  when '2'
+    result = number1 - number2
+  when '3'
+    result = number1 * number2
+  when '4'
+    result = number1 / number2
+  end
+
+  prompt("#{MESSAGES['resultmsg']['eng']} #{result}")
+
+  prompt(MESSAGES['calcagain']['eng'])
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
